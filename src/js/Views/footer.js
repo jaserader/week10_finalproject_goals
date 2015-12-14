7aafda76b2d865b5038ce3498a718c691d06409b
Timothy Andrew Gass <timothyagass@gmail.com>
2015-12-14 14:26:39 -0600

    Fixed some links and added responsiveness to splash page

--- src/js/Views/footer.js
+++ src/js/Views/footer.js
@@ -9,29 +9,29 @@ class Footer extends React.Component{
         <Link to="home" id="logo" href="home"><i className="fa fa-list-ul"></i><span>GOALS</span></Link>
 
         <nav id="socialNav">
-          <Link to=""><i className="fa fa-facebook-official"></i></Link>
-          <Link to=""><i className="fa fa-twitter-square"></i></Link>
-          <Link to=""><i className="fa fa-instagram"></i></Link>
+          <Link to="/"><i className="fa fa-facebook-official"></i></Link>
+          <Link to="/"><i className="fa fa-twitter-square"></i></Link>
+          <Link to="/"><i className="fa fa-instagram"></i></Link>
         </nav>
 
         <div id="footerNav1">
-          <Link>For Business</Link>
-          <Link>Contact Us</Link>
+          <Link to="/">For Business</Link>
+          <Link to="/">Contact Us</Link>
         </div>
 
         <div id="footerNav2">
-          <Link>About Us</Link>
-          <Link>Help</Link>
+          <Link to="/">About Us</Link>
+          <Link to="/">Help</Link>
         </div>
 
         <div id="footerNav3">
-          <Link>Reviews</Link>
-          <Link>Privacy Policy</Link>
+          <Link to="/">Reviews</Link>
+          <Link to="/">Privacy Policy</Link>
         </div>
 
         <div id="footerNav4">
-          <Link>Careers</Link>
-          <Link>Terms of Service</Link>
+          <Link to="/">Careers</Link>
+          <Link to="/">Terms of Service</Link>
         </div>
 
       </footer>
