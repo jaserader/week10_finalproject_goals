48064a340fcf24f11b15aeb928ca0e9e5abd5353
Jase Rader <jaserader@me.com>
2015-12-14 13:51:37 -0600

    added style to sign in and sign up pages

--- src/js/Views/signinpage.js
+++ src/js/Views/signinpage.js
@@ -4,16 +4,18 @@ import { Link } from "react-router";
 class Signinpage extends React.Component {
   render () {
     return (
-      <div id="siginPage">
-      <Link to="home">home</Link>
+      <div id="signinPage">
+
         <form id="signinForm">
           <label for="username" id="usernameLabel">Username</label>
           <input type="text" id="username" />
           <label for="password" id="passwordLabel">Password</label>
           <input type="password" id="password" />
-          <label for="submit" id="submitLabel">Submit</label>
           <input type="submit" id="submit" />
         </form>
+
+        <Link to="signup">sign up</Link>
+
       </div>
     )
   }
--- src/js/Views/signuppage.js
+++ src/js/Views/signuppage.js
@@ -1,27 +1,29 @@
-import React from 'react';
+  import React from 'react';
 import { Link } from "react-router";
 
 class Signuppage extends React.Component {
   render () {
     return (
       <div id="signupPage">
-        <Link to="signin">signin</Link>
+
         <form id="signupForm">
-          <label for="username" id="usernameLabel">Username</label>
-          <input type="text" id="username" />
-          <label for="password" id="passwordLabel">Password</label>
-          <input type="password" id="password" />
-          <label for="passwordConfirm" id="passwordConfirmLabel">Password Confirm</label>
-          <input type="password" id="passwordConfirm" />
-          <label for="first" id="firstLabel">First Name</label>
+          <label for="first" id="firstLabel">First Name:</label>
           <input type="text" id="first" />
-          <label for="last" id="lastLabel">Last Name</label>
+          <label for="last" id="lastLabel">Last Name:</label>
           <input type="text" id="last" />
-          <label for="email" id="emailLabel">Email</label>
+          <label for="username" id="usernameLabel">Username:</label>
+          <input type="text" id="username" />
+          <label for="password" id="passwordLabel">Password:</label>
+          <input type="password" id="password" />
+          <label for="passwordConfirm" id="passwordConfirmLabel">Password Confirm:</label>
+          <input type="password" id="passwordConfirm" />
+          <label for="email" id="emailLabel">Email:</label>
           <input type="text" id="email" />
-          <label for="submit" id="submitLabel">Submit</label>
           <input type="submit" id="submit" />
         </form>
+
+        <Link to="signin">sign in</Link>
+
       </div>
     )
   }
